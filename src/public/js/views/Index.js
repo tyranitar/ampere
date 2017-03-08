3fefa80f4957fe0d63af84d791639829a487db00
Tyron Jung <tyronjung@hotmail.com>
2017-03-08 17:31:02 -0500

    Drawing timeout

--- src/public/js/views/Index.js
+++ src/public/js/views/Index.js
@@ -2,10 +2,45 @@ import React from 'react';
 
 import Canvas from '../canvas/Canvas';
 
+const drawingInterval = 2000;
+
+const getCanvasProps = () => {
+    let drawing = false;
+    let timeout = null;
+
+    const canvasProps = {
+        onDown: (pos) => {
+            if (drawing) {
+                clearTimeout(timeout);
+            } else {
+                console.log("started drawing");
+
+                drawing = true;
+            }
+        },
+
+        onMove: (pos) => {
+            //
+        },
+
+        onUp: (pos) => {
+            timeout = setTimeout(() => {
+                console.log("stopped drawing");
+
+                drawing = false;
+            }, drawingInterval);
+        }
+    };
+
+    return canvasProps;
+};
+
 export default class Index extends React.Component {
     render() {
+        const canvasProps = getCanvasProps();
+
         return (
-            <Canvas></Canvas>
+            <Canvas { ...canvasProps }></Canvas>
         );
     }
 }
