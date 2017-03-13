06b1aca192a5438d554f87672d17fba0302b9ed9
Tyron Jung <tyronjung@hotmail.com>
2017-03-13 04:19:43 -0400

    Front end router fixed

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,21 +1,19 @@
-import { createHistory } from 'history';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, useRouterHistory } from 'react-router';
+import { Router, Route, hashHistory } from 'react-router';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import Index from './views/Index';
 
-const history = useRouterHistory(createHistory)();
 const app = document.getElementById('app');
 
 injectTapEventPlugin();
 
 ReactDOM.render(
     <MuiThemeProvider>
-        <Router history={ history }>
-            <Route path='/' component={ Index }></Route>
+        <Router history={ hashHistory }>
+            <Route path='/' component={ Index } />
         </Router>
     </MuiThemeProvider>,
 app);
